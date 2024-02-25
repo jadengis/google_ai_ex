fe3b4946c2bcd96b7466efa920dc6ea23ae2a017
John Dengis <john@deng.is>
2024-02-24 16:41:38 -0800

    chore: bump version for release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule GoogleAI.MixProject do
   use Mix.Project
 
-  @version "0.1.0"
+  @version "0.1.1"
   @description "Community maintained Google AI API client."
   @source_url "https://github.com/jadengis/google_ai_ex/"
 
